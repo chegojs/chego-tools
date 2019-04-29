d88a4bf8650593b195e281611e2e67461648c860
rkamysz <rkamysz@goodgamestudios.com>
2019-04-29 14:03:12 +0200

    pre publish commit

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
 # chego-tools
+
+This package contains a bunch of functions used across Chego components. 
+
+## Install
+```
+npm install --save @chego/chego-tools
+```
+
+## Short introduction
+
+The functions included in this package have been separated from the core component, so that they can be used in database drivers without any unnecessary and heavy dependencies. As such, they should not be part of the official API.
+
+To find out what `Property`, `Table`, `SortingData`, `Alias`, `RowId`, `Limit`, `LogicalOperatorScope`, `FunctionData` are, please check [Chego API](https://github.com/chegojs/chego-api)
+
+## Tools
+
+### <code>newProperty({ table?: Table, name?: string, alias?: string, type?: QuerySyntaxEnum }): Property</code>
+Returns new `Property` object.
+
+### <code>newTable(name: string, alias?: string): Table</code>
+Returns new `Table` object.
+
+### <code>newLimit(offsetOrCount: number, count?: number): Limit</code>
+Returns new `Limit` object.
+
+### <code>newSortingData(property: Property = null, order: SortingOrderEnum = SortingOrderEnum.ASC): SortingData</code>
+Returns new `SortingData` object.
+
+### <code>rowId(alias: string = 'id'): Property</code>
+Returns new `Property` object with the defined type `QuerySyntaxEnum.RowId`. 
+
+### <code>alias(name: string, alias: string): Property</code>
+Returns new `Property` object with the defined type `QuerySyntaxEnum.Alias`. 
+
+### <code>getLabel(obj: Property | Table):string</code>
+Returns label string of given property/table. If `alias` is set then it will return its value, otherwise it returns `name` of the property.
+
+### <code>parseStringToPropertyIfRequired(key: StringOrProperty):Property</code>
+Function checks if a given `key` is a `string`, if so, it parses it to a `Property` object. 
+
+### <code>mergePropertiesWithLogicalAnd(properties: PropertyOrLogicalOperatorScope[], current: PropertyOrLogicalOperatorScope, i: number): PropertyOrLogicalOperatorScope[]</code>
+`Array.reduce` callback, which checks each element and wraps it within the logical operator scope.
+
+pseudocode 
+```
+select['foo','bar','baz', or('qux')] ---> select['foo',and('bar'),and('baz'), or('qux')] 
+```
+
+### <code>newLogicalOperatorScope(type: QuerySyntaxEnum, properties?: PropertyOrLogicalOperatorScope[]): LogicalOperatorScope</code>
+Returns new `LogicalOperatorScope` object.
+
+### <code>isLogicalOperator(value: QuerySyntaxEnum): boolean</code>
+Checks if a given enum value is equal to `And`, `Or` or `Not`.
+
+### <code>isLogicalOperatorScope(data: any): data is LogicalOperatorScope</code>
+Checks if a given prop is a `LogicalOperatorScope` object.
+
+### <code>isFunction(value: any): boolean</code>
+Checks if a given prop is a `function`.
+
+### <code>clone(obj: any):any</code>
+Performs a deep cloning of an object.
+
+### <code>isQuerySchemeElement(obj: any): boolean</code>
+Checks if a given prop is a `QuerySchemeElement` object.
+
+### <code>combineReducers(...reducers: Fn[]) => (previous: any, current: any, index: number, org: any[])</code>
+Executes `Array.reduce` callbacks - one by one - in a single iteration.
+
+### <code>parseStringToTable(name: string, alias?: string): Table</code>
+Converts given string to `Table` object.
+
+### <code>parseStringToProperty(name: string, table?: Table): Property</code>
+Converts given string to `Property` object.
+
+### <code>isTableDotKeyString(value: any): boolean</code>
+Checks if a given param is a `string` and matches pattern eg. `superheros.name`.
+
+### <code>isAliasString(value: any): boolean</code>
+Checks if a given param is a `string` and matches pattern eg. `writers.name AS author`.
+
+### <code>isProperty = (value: any): value is Property</code>
+Checks if a given param is a `Property` object.
+
+### <code>isRowId = (value: any): boolean</code>
+Checks if a given param is a `Property` object with defined type `QuerySyntaxEnum.RowId`.
+
+### <code>isAlias = (value: any): boolean</code>
+Checks if a given param is a `Property` object with defined type `QuerySyntaxEnum.Alias`.
+
+### <code>isMySQLFunction = (value: any): value is FunctionData</code>
+Checks if a given param is a `FunctionData` object.
+
+## License
+
+Copyright (c) 2019 [Chego contributors](https://github.com/orgs/chegojs/people)
+
+Licensed under the [MIT license](LICENSE).
---
+++ src/tools.ts
@@ -0,0 +1,143 @@
+import { Property, QuerySyntaxEnum, Table, Limit, SortingOrderEnum, SortingData, StringOrProperty, FunctionData, PropertyOrLogicalOperatorScope, LogicalOperatorScope, AnyButFunction, IQuerySchemeElement, Fn } from '@chego/chego-api';
+
+export const isTableDotKeyString = (value: any): boolean =>
+    (typeof value === "string") && /^(\w+)\.(\w+)$/.test(value);
+
+export const isAliasString = (value: any): boolean =>
+    (typeof value === "string") && /^(\w+)( AS )(\w+)$/i.test(value);
+
+export const newProperty = ({ table, name, alias, type }: { table?: Table, name?: string, alias?: string, type?: QuerySyntaxEnum }): Property => ({
+    table: table || null,
+    name: name || '',
+    alias: alias || '',
+    type: type || QuerySyntaxEnum.Undefined
+});
+
+export const newTable = (name: string, alias?: string): Table => ({
+    name,
+    alias: alias || ''
+});
+
+export const newLimit = (offsetOrCount: number, count?: number): Limit => ({ offsetOrCount, count });
+
+export const newSortingData = (property: Property = null, order: SortingOrderEnum = SortingOrderEnum.ASC): SortingData => ({
+    property,
+    order
+});
+
+export const getLabel = (obj: Property | Table):string => obj.alias ? obj.alias : obj.name;
+
+export const mergePropertiesWithLogicalAnd = (properties: PropertyOrLogicalOperatorScope[], current: PropertyOrLogicalOperatorScope, i: number): PropertyOrLogicalOperatorScope[] =>
+    properties.concat(isLogicalOperatorScope(current) || i === 0 ? current : newLogicalOperatorScope(QuerySyntaxEnum.And, [current]));
+
+export const newLogicalOperatorScope = (type: QuerySyntaxEnum, properties?: PropertyOrLogicalOperatorScope[]): LogicalOperatorScope =>
+    ({
+        type,
+        properties: properties || []
+    });
+
+
+export const isLogicalOperator = (value: QuerySyntaxEnum): boolean =>
+    value === QuerySyntaxEnum.And || value === QuerySyntaxEnum.Or || value === QuerySyntaxEnum.Not;
+
+export const isLogicalOperatorScope = (data: any): data is LogicalOperatorScope =>
+    data
+    && Object.keys(data).length === 2
+    && (<LogicalOperatorScope>data).type !== undefined
+    && (<LogicalOperatorScope>data).properties !== undefined
+    && isLogicalOperator(data.type);
+
+export const isFunction = (value: any): boolean => typeof value === 'function';
+
+export const clone = (obj: any): any => {
+    if (Array.isArray(obj)) {
+        return obj.reduce((acc: any, key: any) => (acc.push(clone(key)), acc), [])
+    } else {
+        if (!obj || Object.getPrototypeOf(obj) !== Object.prototype) {
+            return obj;
+        }
+        return Object.keys(obj).reduce((acc: any, key: any) => (acc[key] = clone(obj[key]), acc), {})
+    }
+}
+
+export const isQuerySchemeElement = (obj: any): boolean =>
+    obj
+    && Object.keys(obj).length === 2
+    && (<IQuerySchemeElement>obj).type !== undefined
+    && (<IQuerySchemeElement>obj).params !== undefined;
+
+
+export const combineReducers = (...reducers: Fn[]) => (previous: any, current: any, index: number, org: any[]) => {
+    let tempItem: any;
+    let tempArray: any[];
+    if (Array.isArray(previous)) {
+        tempItem = current;
+        reducers.forEach((reduce: Fn) => {
+            tempArray = reduce([], tempItem, index, org);
+            tempItem = tempArray[0];
+        });
+        return [...previous, ...tempArray];
+    } else {
+        tempItem = previous;
+        reducers.forEach((reduce: Fn) => {
+            tempItem = reduce(tempItem, current, index, org);
+        });
+        return tempItem;
+    }
+}
+
+export const parseStringToTable = (name: string, alias?: string): Table => {
+    const table: Table = newTable(name, alias);
+
+    if (isAliasString(table.name)) {
+        const data: string[] = table.name.replace(/ {1,}/g, ' ').split(' AS ');
+        table.name = data[0];
+        table.alias = data[1];
+    }
+
+    if (isTableDotKeyString(table.name)) {
+        const data: string[] = table.name.split('.');
+        table.name = data[0];
+    }
+    return table;
+}
+
+export const parseStringToProperty = (name: string, table?: Table): Property => {
+    const result: Property = newProperty({ table, name });
+
+    if (isAliasString(result.name)) {
+        const data: string[] = result.name.replace(/ {1,}/g, ' ').split(' AS ');
+        result.name = data[0];
+        result.alias = data[1];
+    }
+
+    if (isTableDotKeyString(result.name)) {
+        const data: string[] = result.name.split('.');
+        result.table = newTable(data[0]);
+        result.name = data[1];
+    }
+    return result;
+}
+
+export const isProperty = (value: any): value is Property =>
+    value
+    && Object.keys(value).length === 4
+    && (<Property>value).table !== undefined
+    && (<Property>value).name !== undefined
+    && (<Property>value).alias !== undefined
+    && (<Property>value).type !== undefined;
+
+export const isMySQLFunction = (data: any): data is FunctionData => data
+    && (<FunctionData>data).type !== undefined
+    && (<FunctionData>data).properties !== undefined
+    && (<FunctionData>data).alias !== undefined
+    && (data.type > 35 && data.type < 43)
+    || (data.type > 21 && data.type < 25);
+
+export const isRowId = (value: any): boolean =>
+    isProperty(value)
+    && (<Property>value).type === QuerySyntaxEnum.RowId;
+
+export const isAlias = (value: any): boolean =>
+    isProperty(value)
+    && (<Property>value).type === QuerySyntaxEnum.Alias;
